588fe33d6cba2f8160047ef3ddf0b7149d48c068
Omar Nasir <omar.nasir@helvar.com>
2023-12-29 03:09:19 +0200

    ts fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
 	return (
 		<html lang="en" data-theme='dark'>
 			<body className={inter.className}>
-				<AuthProvider session={session}>
+				<AuthProvider session={session!}>
 					<Providers>{children}</Providers>
 				</AuthProvider>
 			</body>
