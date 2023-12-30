96f88db3a59cff49d965cc49023210c04f783fff
Omar Nasir <omar.nasir@helvar.com>
2023-12-30 03:26:56 +0200

    remove color mode for hydration fix

--- client/providers/chakraProviders.tsx
+++ client/providers/chakraProviders.tsx
@@ -1,13 +1,12 @@
 'use client'
 import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
 import customTheme from "../themeConfig";
 
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <CacheProvider>
-      <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
       <ChakraProvider theme={customTheme}>
         {children}
       </ChakraProvider>
--- components/auth/box-wrapper.tsx
+++ components/auth/box-wrapper.tsx
@@ -11,18 +11,18 @@ import Image from 'next/image'
 import logo from '../../public/logo.png'
 
 export function BoxWrapper(props: BoxProps) {
-  const logoDims = 75;
+  const logoDims = 50;
   return (
     <Container>
       <Flex h='100vh' direction={'column'} justifyContent={'center'}>
-        <Flex justifyContent={'space-evenly'}>
+        <Flex justifyContent={'space-evenly'} mb={3}>
           <Center textAlign={'center'} fontSize={'3xl'} fontWeight={'light'}
             alignSelf={'center'} alignItems={'center'}>taqseem</Center>
           <Divider orientation="vertical" alignSelf={'center'}
             height={'1%'}
             borderWidth={'0.3rem'}
             borderRadius={'2rem'} />
-          <Image priority={true}  src={logo} alt={'taqseem'} height={logoDims} width={0}/>
+          <Image priority={true} src={logo} alt={'taqseem'} height={logoDims} width={0}/>
         </Flex>
         <Flex>
           <Box w={'100%'}
